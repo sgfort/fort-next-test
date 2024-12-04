a1c1dedce5912adddeec5559134ebceaafe2bfd5
Sandeep Kumar Singh <sandeep.singh@fortmindz.in>
2024-12-04 14:45:15 +0530

    texts

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <p className="text-xs">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cum
+        quia blanditiis libero dolor enim, perferendis in aliquid inventore non
+        consequuntur molestiae fugit sint a, ullam debitis? Facere, odit ut.
+      </p>
+      <p className="text-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+        veritatis.
+      </p>
     </div>
   );
 }
